acc5e2a6e653c1944f36ce718cad72f61148bf54
Evertonsilva21 <evertonkuns@gmail.com>
2024-08-14 21:16:43 -0300

    Add files via upload
    exercício iniciante Java.

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+
+
+        System.out.println("A tv está ligada :"+ smartTv.ligada);
+        System.out.println("Canal atual? :" + smartTv.canal);
+        System.out.println("volume atual? : " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo estatus A tv está ligada :"+ smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("Novo estatus A tv está ligada :"+ smartTv.ligada);
+
+
+
+     
+        
+
+
+
+    }
+}
